5d107997553e2b98c9ae3853960e0f28cbe1d41b
PabloPomar <ciberpablo12@hotmail.com>
2021-01-13 20:02:10 -0300

    Agregar archivos de proyecto.

---
+++ Hahn.ApplicatonProcess.December2020.Domain/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hahn.ApplicatonProcess.December2020.Domain.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
